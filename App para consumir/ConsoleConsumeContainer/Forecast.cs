f47f5c5ed948e4ed0d69d51d5d5903fb704cd1b4
RicardoJosuePerezAltamirano <ricardoj.altamirano@outlook.com>
2021-04-24 10:09:22 -0500

    initial

---
+++ App para consumir/ConsoleConsumeContainer/Forecast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleConsumeContainer
+{
+    public class Forecast
+    {
+        public DateTime date { get; set; }
+        public int temperatureC { get; set; }
+        public int temperatureF { get; set; }
+        public string summary { get; set; }
+    }
+}
---
+++ App para consumir/ConsoleConsumeContainer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleConsumeContainer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:4000");
+            var response = await Client.GetFromJsonAsync<List<Forecast>>("/WeatherForecast");
+            if (response != null)
+            {
+                foreach (var item in response)
+                {
+                    Console.WriteLine($"{item.date} {item.summary} {item.temperatureC} {item.temperatureF}");
+                }
+            }
+        }
+    }
+}
